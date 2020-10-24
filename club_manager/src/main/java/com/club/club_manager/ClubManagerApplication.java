bd6bdc952da3abcef922100012a262d627dfc13b
lhz1165 <2190732276@qq.com>
2020-10-24 13:43:31 +0800

    1

---
+++ club_manager/src/main/java/com/club/club_manager/ClubManagerApplication.java
@@ -0,0 +1,13 @@
+package com.club.club_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagerApplication.class, args);
+	}
+
+}
